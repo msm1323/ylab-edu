fa7c90f3339ac71f4ab770b45dfb964e6cdd3614
Svetlana <svetlana.magomedova.1998@gmail.com>
2023-03-29 09:59:38 +0300

    Lesson 3: renaming&simplification [Sorter]

--- src/main/java/ru/msm/edu/lesson_3/FileSort/Sorter.java
+++ src/main/java/ru/msm/edu/lesson_3/FileSort/Sorter.java
@@ -7,28 +7,27 @@ import java.nio.file.Files;
 import java.util.*;
 
 public class Sorter {
-    private int N = 60_000_000;     //60_000_000
+    private int primaryBlocksSize = 60_000_000;     //60_000_000
 
     Sorter() {
     }
 
-    Sorter(int N) {
-        this.N = N;
+    Sorter(int primaryBlocksSize) {
+        this.primaryBlocksSize = primaryBlocksSize;
     }
 
     public File sortFile(File dataFile) throws IOException {
         ArrayList<Long> block = new ArrayList<>();
-        int blocksCounter = 0;
+        int primaryBlocksCounter = 0;
 
-        //разбитие на первичные блоки размера N
         try (Scanner scanner = new Scanner(Files.newInputStream(dataFile.toPath()))) {
             while (scanner.hasNextLong()) {
-                for (int i = 0; i < N && scanner.hasNextLong(); i++) {
+                for (int i = 0; i < primaryBlocksSize && scanner.hasNextLong(); i++) {
                     block.add(scanner.nextLong());
                 }
                 Collections.sort(block);
 
-                File file = new File("_0_" + blocksCounter);
+                File file = new File("_0_" + primaryBlocksCounter);
                 try (PrintWriter pw = new PrintWriter(file)) {
                     for (Long l : block) {
                         pw.println(l);
@@ -37,49 +36,44 @@ public class Sorter {
                 }
 
                 block.clear();
-                blocksCounter++;
+                primaryBlocksCounter++;
             }
         }
 
-        return blocksMerging(0, blocksCounter);
+        return blocksMerging(0, primaryBlocksCounter);
     }
 
     private File blocksMerging(int lastIteration, int lastBlocksNum) throws IOException {
         for (int i = 0; i < lastBlocksNum / 2; i++) {
+            File mergeBlockFile = new File("_" + (lastIteration + 1) + "_" + i);
+
             String block1stFileName = "_" + lastIteration + "_" + i * 2;
             String block2ndFileName = "_" + lastIteration + "_" + (i * 2 + 1);
             try (Scanner scanner1 = new Scanner(Files.newInputStream(new File(block1stFileName).toPath()));
                  Scanner scanner2 = new Scanner(Files.newInputStream(new File(block2ndFileName).toPath()))) {
 
-                File mergeBlockFile = new File("_" + (lastIteration + 1) + "_" + i);
-                Long l1 = scanner1.hasNextLong() ? scanner1.nextLong() : null;
-                Long l2 = scanner2.hasNextLong() ? scanner2.nextLong() : null;
+                Long block1stDigit = scanner1.hasNextLong() ? scanner1.nextLong() : null;
+                Long block2ndDigit = scanner2.hasNextLong() ? scanner2.nextLong() : null;
                 try (PrintWriter pw = new PrintWriter(mergeBlockFile)) {
-                    while (true) {
-                        if (l1 != null && l2 != null) {
-                            if (l1 < l2) {
-                                pw.println(l1);
-                                l1 = scanner1.hasNextLong() ? scanner1.nextLong() : null;
-                            } else {
-                                pw.println(l2);
-                                l2 = scanner2.hasNextLong() ? scanner2.nextLong() : null;
-                            }
-                        } else if (l1 != null) {
-                            pw.println(l1);
-                            while (scanner1.hasNextLong()) {
-                                pw.println(scanner1.nextLong());
-                            }
-                            break;
-                        } else if (l2 != null) {
-                            pw.println(l2);
-                            while (scanner2.hasNextLong()) {
-                                pw.println(scanner2.nextLong());
-                            }
-                            break;
+                    while (block1stDigit != null && block2ndDigit != null) {
+                        if (block1stDigit < block2ndDigit) {
+                            pw.println(block1stDigit);
+                            block1stDigit = scanner1.hasNextLong() ? scanner1.nextLong() : null;
                         } else {
-                            break;
+                            pw.println(block2ndDigit);
+                            block2ndDigit = scanner2.hasNextLong() ? scanner2.nextLong() : null;
                         }
-
+                    }
+                    if (block1stDigit != null) {
+                        pw.println(block1stDigit);
+                        while (scanner1.hasNextLong()) {
+                            pw.println(scanner1.nextLong());
+                        }
+                    } else if (block2ndDigit != null) {
+                        pw.println(block2ndDigit);
+                        while (scanner2.hasNextLong()) {
+                            pw.println(scanner2.nextLong());
+                        }
                     }
                     pw.flush();
                 }
@@ -94,18 +88,18 @@ public class Sorter {
 
         }
 
-        int curBlocksCounter = lastBlocksNum / 2 + lastBlocksNum % 2;
+        int curBlocksNum = lastBlocksNum / 2 + lastBlocksNum % 2;
 
         if (lastBlocksNum % 2 == 1) {
             File file = new File("_" + lastIteration + "_" + (lastBlocksNum - 1));
-            File newNamedFile = new File("_" + (lastIteration + 1) + "_" + (curBlocksCounter - 1));
+            File newNamedFile = new File("_" + (lastIteration + 1) + "_" + (curBlocksNum - 1));
             if (!file.renameTo(newNamedFile)) {
                 throw new IOException("Не удалось переименовать файл " + file.getPath() + " в " + newNamedFile.getPath());
             }
         }
 
-        if (curBlocksCounter != 1) {
-            return blocksMerging(lastIteration + 1, curBlocksCounter);
+        if (curBlocksNum != 1) {
+            return blocksMerging(lastIteration + 1, curBlocksNum);
         } else {
             File file = new File("_" + (lastIteration + 1) + "_0");
             File newNamedFile = new File("sortedData" + (new Date()).getTime());
